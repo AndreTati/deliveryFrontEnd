faf18e921ece850263792307b131a70836ef8367
lusho99 <38288556+lusho99@users.noreply.github.com>
2019-04-20 22:22:11 -0300

    ABM Articulo Categoria + Estructuracion

--- src/app/servicios/data-api.service.ts
+++ src/app/servicios/data-api.service.ts
@@ -3,8 +3,9 @@ import { HttpClient} from '@angular/common/http';
 import {productosInteface} from '../componentes/catalogo/lista-productos/item-productos/productosInterface';
 import { Observable } from 'rxjs';
 import {categoriasInterface} from '../componentes/catalogo/filtro/categorias-filtro/categoriasInterface';
-import {ProvinciasInterface} from "../componentes/usuarios/register/ProvinciasInterface";
-import { LocalidadesInterface} from "../componentes/usuarios/register/LocalidadesInterface";
+import {ProvinciasInterface} from '../componentes/usuarios/register/ProvinciasInterface';
+import { LocalidadesInterface} from '../componentes/usuarios/register/LocalidadesInterface';
+import {articuloCategoriaInterface} from '../componentes/usuarioAdministrador/articulocategoria/articuloCategoriaInterface';
 
 @Injectable({
   providedIn: 'root'
@@ -12,22 +13,27 @@ import { LocalidadesInterface} from "../componentes/usuarios/register/Localidade
 export class DataApiService {
 
   constructor(public httpClientApi: HttpClient) { }
- private _urlAllPlatos : string ="http://apirestdelivery.herokuapp.com/api/v1/plato/";
- private _urlAllCategorias: string ="http://apirestdelivery.herokuapp.com/api/v1/platocategoria/";
- private _urlAllProvincias: string ="http://apirestdelivery.herokuapp.com/api/v1/provincia/";
-  private _urlAllLocalidades: string ="http://apirestdelivery.herokuapp.com/api/v1/localidad/";
-
- getAllPlatos (): Observable<productosInteface[]> {
+ private _urlAllPlatos = 'http://apirestdelivery.herokuapp.com/api/v1/plato/';
+ private _urlAllCategorias = 'http://apirestdelivery.herokuapp.com/api/v1/platocategoria/';
+ private _urlAllProvincias = 'http://apirestdelivery.herokuapp.com/api/v1/provincia/';
+ private _urlAllLocalidades = 'http://apirestdelivery.herokuapp.com/api/v1/localidad/';
+ private _urlAllArticuloCategoria = 'http://apirestdelivery.herokuapp.com/api/v1/articulocategoria/';
+  getAllPlatos(): Observable<productosInteface[]> {
     return this.httpClientApi.get<productosInteface[]>(this._urlAllPlatos);
   }
-  getAllCategorias (): Observable<categoriasInterface[]> {
+  getAllCategorias(): Observable<categoriasInterface[]> {
     return this.httpClientApi.get<categoriasInterface[]>(this._urlAllCategorias);
   }
 
-  getAllProvincias (): Observable<ProvinciasInterface[]> {
+  getAllProvincias(): Observable<ProvinciasInterface[]> {
     return this.httpClientApi.get<ProvinciasInterface[]>(this._urlAllProvincias);
   }
-  getAllLocalidades (): Observable<LocalidadesInterface[]> {
+  getAllLocalidades(): Observable<LocalidadesInterface[]> {
     return this.httpClientApi.get<LocalidadesInterface[]>(this._urlAllLocalidades);
   }
+  getAllArticuloCategoria(): Observable <articuloCategoriaInterface[]> {
+   return this.httpClientApi.get<articuloCategoriaInterface[]>(this._urlAllArticuloCategoria);
+  }
+
+
 }
